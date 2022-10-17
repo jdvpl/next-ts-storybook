9f49242cc19ef9f139b49493b9e5c0729e01d8f4
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-10-16 19:15:45 -0500

    testing bug solved

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,23 @@
 const nextJest = require("next/jest");
-/* Creating a jest config object. */
+
 const createJestConfig = nextJest({
-  /* Telling the nextJest function where to look for the next.config.js file. */
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
-/* The above code is setting up the Jest configuration for the project. */
+// Add any custom config to be passed to Jest
 const customJestConfig = {
+  // Add more setup options before each test is run
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    // Handle module aliases (this will be automatically configured for you soon)
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  collectCoverage: true,
+  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
 };
 
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 module.exports = createJestConfig(customJestConfig);
